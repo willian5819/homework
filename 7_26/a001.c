e75e12840db5be5bbb47c97074e796b29899d83b
willian5819 <87977942+willian5819@users.noreply.github.com>
2021-07-26 19:01:40 +0800

    Create a001.c

---
+++ 7_26/a001.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main()
+{
+
+
+int i =0;
+
+
+for (int j =0;j<10;j++)
+
+{
+
+    i++;
+
+
+
+
+
+
+}
+
+
+
+
+
+return 0;
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
